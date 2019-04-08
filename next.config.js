08662fd99b9180a931fbb3da61ffa16d07cd7a5b
Sam Carlton <sam@sam.lc>
2019-04-07 20:03:15 -0500

    Add next.config

---
+++ next.config.js
@@ -0,0 +1,28 @@
+// const webpack = require('webpack')
+
+/**
+ * After the next require you can use process.env to get your secrets
+ */
+require('now-env')
+
+/**
+ * If some of the envs are public, like a google maps key, but you still
+ * want to keep them secret from the repo, the following code will allow you
+ * to share some variables with the client, configured at compile time.
+ */
+ 
+// const config = config => {
+//   config.plugins.push(
+//     new webpack.DefinePlugin({
+//       // Shared with front-end
+//       'process.env.APP_NAME': JSON.stringify(process.env.APP_NAME),
+//       'process.env.GOOGLE_KEY': JSON.stringify(process.env.GOOGLE_KEY),
+//       'process.env.FACEBOOK_APP_ID': JSON.stringify(process.env.FACEBOOK_APP_ID)
+//     })
+//     // Same as above
+//     // new webpack.EnvironmentPlugin(['SECRET'])
+//   )
+//   return config
+// }
+ 
+module.exports = {}
