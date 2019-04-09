b6ecd5dc7035274ca7041a3c30337a169e2f40c0
Sam Carlton <sam@sam.lc>
2019-04-08 22:45:51 -0500

    Break ig posts out of edges

--- helpers/ig/getPosts.js
+++ helpers/ig/getPosts.js
@@ -4,4 +4,9 @@ module.exports =  async function (username) {
     const client = await getClient()
 
     return await client.getPhotosByUsername({ username })
+        .then(data => {
+            const edges = data.user.edge_owner_to_timeline_media.edges
+            return edges
+        })
+        .catch(console.log)
 }
