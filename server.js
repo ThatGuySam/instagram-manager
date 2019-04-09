78619c42022f4d1b04194c46afa9675a96870b34
Sam Carlton <sam@sam.lc>
2019-04-08 23:20:10 -0500

    Set up posting route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const next = require('next')
 
+const postToInstagram = require('./helpers/ig/post')
+const getNewRedditPosts = require('./helpers/getNewRedditPosts')
+
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -9,17 +12,24 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
+  server.get('/post', async (req, res) => {
+
+    const redditPosts = await getNewRedditPosts()
+
+    const post = redditPosts[0]
+
+    // console.log('post', post)
+
+    const response = await postToInstagram(post.data.url, post.data.title)
+
+    // console.log('response', response)
+    
+    res.send(response)
   })
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
-
-  server.get('/posts/:id', (req, res) => {
-    return app.render(req, res, '/posts', { id: req.params.id })
-  })
+  // server.get('/posts/:id', (req, res) => {
+  //   return app.render(req, res, '/posts', { id: req.params.id })
+  // })
 
   server.get('*', (req, res) => {
     return handle(req, res)
