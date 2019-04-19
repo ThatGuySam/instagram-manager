70dfcf8f642980e1e88932ac4df92fe7080b8a1c
Sam Carlton <sam@sam.lc>
2019-04-18 20:18:32 -0500

    Pull memes from opvotes list

--- helpers/getDankChristianMemes.js
+++ helpers/getDankChristianMemes.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 module.exports =  async function () {
     let data = null
     
-    await axios.get('https://www.reddit.com/r/dankchristianmemes/new.json')
+    await axios.get('https://www.reddit.com/user/stolenchristianmemes/upvoted.json')
         .then(res => data = res.data)
         .catch(console.log)
     
