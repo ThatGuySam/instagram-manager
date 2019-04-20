1598b8d77fdddc8f29076b19fcd020391499ff77
Sam Carlton <sam@sam.lc>
2019-04-19 23:20:05 -0500

    Fix duplicate posts

---
+++ helpers/postMeme.js
@@ -0,0 +1,17 @@
+const postToInstagram = require('./ig/post')
+const postedMemes = require('./postedMemes')
+
+module.exports =  async function (post) {
+
+    // Add it to postedMemes
+    postedMemes.store(post)
+    
+    const postTitle = post.data.title
+    const postAuthor = post.data.author
+    const caption = `${postTitle} ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+    Stolen from u/${postAuthor}`
+    
+    const response = await postToInstagram(post.data.url, caption)
+    
+    return response
+}
