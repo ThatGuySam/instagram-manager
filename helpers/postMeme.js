e06eb4a9b6cafc92ad2faa06820d6c2c9559b4f4
Sam Carlton <sam@sam.lc>
2019-04-20 00:52:20 -0500

    Use period for caption

--- helpers/postMeme.js
+++ helpers/postMeme.js
@@ -1,5 +1,6 @@
 const postToInstagram = require('./ig/post')
 const postedMemes = require('./postedMemes')
+// const makeCaption = require('./makeCaption')
 
 module.exports =  async function (post) {
 
@@ -8,8 +9,7 @@ module.exports =  async function (post) {
     
     const postTitle = post.data.title
     const postAuthor = post.data.author
-    const caption = `${postTitle} ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
-    Stolen from u/${postAuthor}`
+    const caption = `${postTitle} . Stolen from u/${postAuthor}`
     
     const response = await postToInstagram(post.data.url, caption)
     
