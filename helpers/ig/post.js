ec8a16082dfbd77a802d043657749d4134018de7
Sam Carlton <sam@sam.lc>
2019-05-01 17:11:25 -0500

    Wrap ig posting in try catch

--- helpers/ig/post.js
+++ helpers/ig/post.js
@@ -4,7 +4,15 @@ module.exports =  async function (photo, caption) {
     // Get the client
     const client = await getClient()
     // Get own profile
-    const response = await client.uploadPhoto({ photo, caption })
+
+    let response
+
+    try {
+        response = await client.uploadPhoto({ photo, caption })
+    } catch (error) {
+        console.log('Error posting to Instagram', error)
+        throw new Error(error)
+    }
     
     return response
 }
