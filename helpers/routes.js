0ef05433b6865efc2d8a70cad4f4ac869f27b671
Sam Carlton <sam@sam.lc>
2019-08-26 09:56:03 -0500

    Fix postImage route

--- helpers/routes.js
+++ helpers/routes.js
@@ -1,3 +1,3 @@
 module.exports =  {
-    'postImage': (postId) => `${process.env.SCREENSHOT_ENDPOINT}${redditPost.data.name}.jpg?quality=100`
+    'postImage': (postId) => `${process.env.SCREENSHOT_ENDPOINT}${postId}.jpg?quality=100`
 }
